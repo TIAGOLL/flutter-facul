d619c66d72d74e805883ff9a3e1bc53cd8c957df
Tiago Emanuel de Lima <tiagoepitanga10@gmail.com>
2024-09-05 20:47:52 -0300

    update

--- jogo2/lib/jogo.dart
+++ jogo2/lib/jogo.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'dart:math';
 
 class Jogo extends StatefulWidget {
@@ -79,5 +78,61 @@ class _JogoState extends State<Jogo> {
           });
         }
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Escolha do App",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )),
+          Image(image: this._imagemApp),
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                this._mensagem,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("pedra"),
+                child: Image.asset(
+                  "imagens/pedra.png",
+                  height: 100,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("papel"),
+                child: Image.asset(
+                  "imagens/papel.png",
+                  height: 100,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("tesoura"),
+                child: Image.asset(
+                  "imagens/tesoura.png",
+                  height: 100,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- jogo2/lib/main.dart
+++ jogo2/lib/main.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:jogo2/jogo.dart';
 
 void main() {
-  runApp(const MyApp(
-    MaterialApp(
-      home: jogo(),
-      debugShowCheckedModeBanner: false,
-    )
+  runApp(MaterialApp(
+    home: Jogo(),
+    debugShowCheckedModeBanner: false,
   ));
 }
-
